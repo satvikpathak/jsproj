ff634436ad1a39a2da47d7bc214e75d29949987f
Satvik Pathak <satvikpathak007@gmail.com>
2024-10-31 12:42:19 +0530

    Delete api directory

--- api/music-api/routes/playlistRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const playlistController = require('../controllers/playlistController');
-
-router.post('/playlists', playlistController.createPlaylist);
-router.post('/playlists/:playlistId/songs/:songId', playlistController.addSongToPlaylist);
-
-module.exports = router;
